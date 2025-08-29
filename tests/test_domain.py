aedc124ea0db31d8eb5b078c1be2f0e17b885ddb
Emma Marshall <emarshall@rhg.com>
2025-08-29 13:29:23 -0600

    fix format again

--- tests/test_domain.py
+++ tests/test_domain.py
@@ -1,18 +1,15 @@
 from toy_glacier import domain
 
-def test_accumulate():
 
-    glacier = domain.Glacier(name='test1',
-                             mass = 100)
+def test_accumulate():
+    glacier = domain.Glacier(name="test1", mass=100)
     glacier.accumulate(20)
 
     assert glacier.mass == 120
 
-def test_ablate():
 
-    glacier = domain.Glacier(name='test',
-                             mass=100)
+def test_ablate():
+    glacier = domain.Glacier(name="test", mass=100)
     glacier.ablate(50)
 
     assert glacier.mass == 50
-
